908eb68c119d4a412a35717555e33d08d21bfd11
zhangchunsheng <zhangchunsheng423@gmail.com>
2014-03-22 21:16:46 +0800

    1.重构：调整项目代码

---
+++ TestParseHTML/TestParseHTML/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  TestParseHTML
+//
+//  Created by Adam on 13-10-8.
+//  Copyright (c) 2013年 Adam. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ TestParseHTML/TestParseHTML/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  TestParseHTML
+//
+//  Created by Adam on 13-10-8.
+//  Copyright (c) 2013年 Adam. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
